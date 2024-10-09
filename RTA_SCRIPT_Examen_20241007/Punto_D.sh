8a7a20d3560449eb7ef650e16c99562ad6891931
Sergio-Dario-Moyano <sergiodariomoyano@gmail.com>
2024-10-09 22:25:49 +0000

    FIX: Se arregló un error tipográfico en el ejercicio Punto_D.sh

--- RTA_SCRIPT_Examen_20241007/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_D.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Creando estructura de directorio"
-sudo mkdir -p /Estructura_Asimetrica/{{correo,clientes}/cartas_{1..100},correo/carteros_{1..10}}
+sudo mkdir -p /Estructura_Asimetrica/{{correo,cliente}/cartas_{1..100},correo/carteros_{1..10}}
 
 echo "Validamos la estructura"
 tree /Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --columns 4
