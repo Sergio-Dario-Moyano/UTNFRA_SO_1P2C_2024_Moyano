ef205b592597cc230693bb490b664ea83d611ab8
Sergio-Dario-Moyano <sergiodariomoyano@gmail.com>
2024-10-09 22:02:39 +0000

    ADD: Agregado el ejercicio del Punto_F.sh

---
+++ RTA_SCRIPT_Examen_20241007/Punto_F.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "Datos"
+MI_IP=$(curl -s ifconfig.me)
+MI_USUARIO=$(whoami)
+MI_HASH=$(sudo grep -i vagrant /etc/shadow | awk -F ':' '{print $2}')
+MI_URL=$(sudo git remote get-url origin)
+
+
+
+echo "Mi IP pública es: "$MI_IP > RTA_ARCHIVOS_Examen_20241009/Filtro_Avanzado.txt 
+echo "Mi Usuario es: "$MI_USUARIO >> RTA_ARCHIVOS_Examen_20241009/Filtro_Avanzado.txt
+echo "El HASH de mi usuario es:" $MI_HASH >> RTA_ARCHIVOS_Examen_20241009/Filtro_Avanzado.txt
+echo "La URL de mi repositorio es: "$MI_URL >> RTA_ARCHIVOS_Examen_20241009/Filtro_Avanzado.txt
+
+
